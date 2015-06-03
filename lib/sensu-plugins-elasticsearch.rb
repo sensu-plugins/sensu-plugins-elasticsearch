1dfed69e1bde7349d7b9710038054f2685947d85
Matt Jones <mattjones@yieldbot.com>
2015-06-02 23:18:11 -0400

    deploy

--- lib/sensu-plugins-elasticsearch.rb
+++ lib/sensu-plugins-elasticsearch.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-elasticsearch/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsElasticsearch
-  class << self
-  end
-
-  class << self
-  end
-end
